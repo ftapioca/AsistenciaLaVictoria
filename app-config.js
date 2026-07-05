5d99ee2bc9e8822c9034bec82c9b11390003931e
ftapioca <ftapioca@gmail.com>
2026-07-05 17:15:48 -0400

    Fix staging Apps Script deployment URL typo

--- app-config.js
+++ app-config.js
@@ -9,7 +9,7 @@
     },
     staging: {
       ENVIRONMENT: "staging",
-      WEB_APP_URL: "https://script.google.com/macros/s/AKfycbzcfyIN11hOygphJChfCyPGsj4Th-CfL8ZqF0k7_N-afJZeKZphqFPUrPpBXsvtY-5nFA/exec",
+      WEB_APP_URL: "https://script.google.com/macros/s/AKfycbzcfyIN11hOygphJChfCyPGsj4Th-CfL8ZqFOk7_N-afJZeKZphqFPUrPpBXsvtY-5nFA/exec",
       SESSION_KEY: "lavictoria.auth.session.staging"
     }
   };
--- app-config.staging.js
+++ app-config.staging.js
@@ -2,6 +2,6 @@ window.APP_CONFIG_PRESETS = window.APP_CONFIG_PRESETS || {};
 
 window.APP_CONFIG_PRESETS.staging = {
   ENVIRONMENT: "staging",
-  WEB_APP_URL: "https://script.google.com/macros/s/AKfycbzcfyIN11hOygphJChfCyPGsj4Th-CfL8ZqF0k7_N-afJZeKZphqFPUrPpBXsvtY-5nFA/exec",
+  WEB_APP_URL: "https://script.google.com/macros/s/AKfycbzcfyIN11hOygphJChfCyPGsj4Th-CfL8ZqFOk7_N-afJZeKZphqFPUrPpBXsvtY-5nFA/exec",
   SESSION_KEY: "lavictoria.auth.session.staging"
 };
