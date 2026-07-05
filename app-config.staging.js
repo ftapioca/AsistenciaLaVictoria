5ac75a3aa872174aceb44b568f77766302e04384
ftapioca <ftapioca@gmail.com>
2026-07-05 15:44:31 -0400

    Point staging frontend to current Apps Script deployment

--- app-config.staging.js
+++ app-config.staging.js
@@ -2,6 +2,6 @@ window.APP_CONFIG_PRESETS = window.APP_CONFIG_PRESETS || {};
 
 window.APP_CONFIG_PRESETS.staging = {
   ENVIRONMENT: "staging",
-  WEB_APP_URL: "https://script.google.com/macros/s/AKfycbw31mYudOa7UpNfdYNfv0tg0DnWnc5trTgV0FLm3SDNJUwY0QxFzqrTbDG9uSTsagx5/exec",
+  WEB_APP_URL: "https://script.google.com/macros/s/AKfycbzcfyIN11hOygphJChfCyPGsj4Th-CfL8ZqF0k7_N-afJZeKZphqFPUrPpBXsvtY-5nFA/exec",
   SESSION_KEY: "lavictoria.auth.session.staging"
 };
